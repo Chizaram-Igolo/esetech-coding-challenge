dd595e4368e2f2730d20cb7cd4c69ab0e5b32632
Chizaram Igolo <emmanuelchizaram@verydeeptech.com>
2024-01-25 13:18:15 +0100

    🏗️ Set Up Project Tools, Create Firebase Project and Install Firebase SDK

---
+++ server/firebase/firebase.init.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDHfX2VmuMYEHmyMk6gnH01Rk5m6xSfXHI",
+  authDomain: "esetech-coding-challenge-13fb6.firebaseapp.com",
+  projectId: "esetech-coding-challenge-13fb6",
+  storageBucket: "esetech-coding-challenge-13fb6.appspot.com",
+  messagingSenderId: "883514773414",
+  appId: "1:883514773414:web:c68b4ffdbaf476b446c967",
+  measurementId: "G-M186Z8Q3M8",
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
